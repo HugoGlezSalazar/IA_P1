6b23a3b3f96b98c05ed67f57da9b09b2861d8289
HugoGlezSalazar <143573857+HugoGlezSalazar@users.noreply.github.com>
2023-09-14 17:42:35 -0600

    1_Suma_Cap2
    Este primer programa te enseña como imprimir un mensaje al igual que realizar una suma

---
+++ 1_Suma_Cap2.py
@@ -0,0 +1,17 @@
+mensaje = "Resultado de tu suma."
+num1 = 15
+num2 = 20
+
+num1 = 15
+num2 = 20
+
+resultado = num1 + num2
+num1 = 15
+nume2 = 20
+
+resultado = num1 + num2
+
+print(resultado)
+
+print(mensaje)
+
