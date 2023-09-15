a9861dd1750503439da7d236dee3173b7b3ceee9
HugoGlezSalazar <143573857+HugoGlezSalazar@users.noreply.github.com>
2023-09-14 18:43:55 -0600

    Pc2_imprecion con comillas_Cap3
    En esta practica tienes que usar las comillas simples (´) y las dos comillas (")

---
+++ Pc2_imprecion con comillas_Cap3.py
@@ -0,0 +1,10 @@
+texto1 = 'Este es el string en comillas simples.'
+
+texto2 = "Este es el string en comillas dobles."
+
+frase = '"print()" se utiliza para imprimir valores en la consola.'
+
+print(frase)
+
+
+
