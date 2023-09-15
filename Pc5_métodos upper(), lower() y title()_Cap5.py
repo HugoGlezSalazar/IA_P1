07a024da0ae80c55ff6304a11422d92b9ff4f067
HugoGlezSalazar <143573857+HugoGlezSalazar@users.noreply.github.com>
2023-09-14 19:27:42 -0600

    Pc5_métodos upper(), lower() y title()_Cap5
    métodos upper(), lower() y title() te ayudan a cambiar tus oración ya sea ponerla en minúsculas, mayúsculas o solo la primera letra con mayúscula

---
+++ Pc5_métodos upper(), lower() y title()_Cap5.py
@@ -0,0 +1,7 @@
+#Hugo Alejandro Gozalez Salazar
+
+nombre = "Hugo Alejandro Gozalez Salazar".title()
+
+nombre = "Esta Es Una Frase Para Ser Formateada.".lower()
+
+nombre = "Esta Es Una Frase Para Ser Formateada.".upper()
